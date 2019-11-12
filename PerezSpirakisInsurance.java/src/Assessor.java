56256e7182b5fa91c49fb22bf13848e7e52e2f97
7pereza <adrianjperez@lewisu.edu>
2019-11-12 12:13:36 -0600

    working on assessor
    just works on some function so far

--- PerezSpirakisInsurance.java/src/Assessor.java
+++ PerezSpirakisInsurance.java/src/Assessor.java
@@ -7,7 +7,7 @@
 
 import java.util.ArrayList;
 
-public class Assessor {
+public class Assessor {//this is proprly  going to be a static class
 	
 	private ArrayList<Member> members;//hold the list of the members
 	/*
@@ -18,14 +18,11 @@ public class Assessor {
 	 * */
 	private String [][] riskMembers; //this will hold the member data on rather there are a risk of not  
 	
-		
-	
 	
 	public Assessor() {
 		members = null;	
 		riskMembers = null;
-	}
-	
+	}	
 
 	public Assessor(ArrayList<Member> mems) {
 		members = mems;
@@ -34,18 +31,44 @@ public class Assessor {
 	}
 	
 	
-	//function for the BMI
-	public int calBMI(int h, int w) {
+	//function for the BMI return a string 		
+	public String elvBMI(int height, int weight) {
+		//the unit system is in Imperial units (lb and inches )
+		double  bmi =((weight*703) / Math.pow(height,2));
 		
-		return 0;
-	}	
-	
-	public string elvBMI() {}
+		if( bmi <= 24.9 ) {
+			return "normal";
+		}else if(  25.0<=bmi || bmi<=29.9 ) {			
+			return "overweight";
+		}else {//30.0-34.9			
+			return "obesity";
+		}		
+	}
 	
 	//function for bloodpressure 
-	public String elvBP(int s, int d ) {
+	public String elvBP(int sys, int dia ) {
+				////have to change the print stat into return
+		//hav eto check the logic
+		//need to reuten normal, elevated,stage1, stage2, crisis
 		
-		return "";
+		if(sys < 120 && dia < 80) 
+		{
+			System.out.println("Normal");
+		}
+		else if( (120<=sys && sys<=139) | (80<=dia && dia<=89)    ) 
+		{
+			System.out.println("Prehypertension");
+		}
+		
+		//add satge two 
+		
+		else if ( 140<=sys | 90<=dia  ) 
+		{
+			System.out.println("High Blood Pressure");
+		}	
+		//add cirst 
+		
+		
 	}
 	
 	public int totalPoints(String BMI, String BP, int age, boolean c, boolean d, boolean a ) {
@@ -55,7 +78,7 @@ public class Assessor {
 	}
 	
 	//function to pick the risk name and saving to the list 
-	public void eval() {
+	public void evalAll() {
 		int BMI;
 		String BP;
 		int point;//??
@@ -79,7 +102,7 @@ public class Assessor {
 	public void printRisk() {
 		
 		
-	}
+	}	
 	
 	
 	
@@ -87,9 +110,4 @@ public class Assessor {
 	
 	
 	
-	
-	
-	
-	
-	
-}
+}//end of class
